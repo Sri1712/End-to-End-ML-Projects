269db149e2984fa0aaeecac8e48d457312d6b756
Srilathaa Vasu <sripriyavasu2004@gmail.com>
2025-12-11 18:22:56 +0100

    Data Validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bikeSharing import logger
 from bikeSharing.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from bikeSharing.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -9,5 +10,17 @@ try:
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataValidationTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+   logger.exception(e)
+   raise e
