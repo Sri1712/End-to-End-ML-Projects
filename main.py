6a0d4f5c0469996dfeabfdc9f3f14670da696535
Srilathaa Vasu <sripriyavasu2004@gmail.com>
2025-12-11 15:48:16 +0100

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.bikeSharing import logger
+from bikeSharing import logger
+from bikeSharing.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Testing logger object")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
